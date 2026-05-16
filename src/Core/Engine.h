b00f6a78e7ea166ab7ac9a2a4c1faa37ea497966
clovesrodrigues <75814527+clovesrodrigues@users.noreply.github.com>
2026-05-16 01:02:30 -0300

    Restore Engine to initial logo splash commit

--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -31,6 +31,9 @@ public:
 
 private:
     bool CreateDevice();
+    void LoadLogoTexture();
+    void DrawStartupSplash();
+    void DrawScreenLogo();
     void StartNewGame();
 
     std::string mediaDir_;
@@ -39,6 +42,7 @@ private:
     irr::scene::ISceneManager* sceneManager_ = nullptr;
     irr::gui::IGUIEnvironment* guiEnvironment_ = nullptr;
     irr::u32 lastFrameTimeMs_ = 0;
+    irr::video::ITexture* logoTexture_ = nullptr;
 
     BoardManager boardManager_;
     ChessSceneManager chessSceneManager_;
