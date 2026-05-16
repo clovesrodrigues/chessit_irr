803ebe3cd356c19d182bd85126bfa02e5f033d97
clovesrodrigues <75814527+clovesrodrigues@users.noreply.github.com>
2026-05-15 23:51:59 -0300

    Update Engine.h

--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -1,16 +1,11 @@
-diff --git a/src/Core/Engine.h b/src/Core/Engine.h
-index 1c8c3c648a14442dad41dbc92031b8800beec63d..8d06d3e63f2f9b0392a33ecfb1945375d5aa00b3 100644
---- a/src/Core/Engine.h
-+++ b/src/Core/Engine.h
-@@ -9,47 +9,51 @@
- #include "Managers/PieceManager.h"
- #include "Managers/SaveReplaySystem.h"
- #include "Managers/SoundManager.h"
- #include "Managers/UIManager.h"
+#include "Managers/PieceManager.h"
+#include "Managers/SaveReplaySystem.h"
+#include "Managers/SoundManager.h"
+#include "Managers/UIManager.h"
  
- #include <irrlicht.h>
- #include <memory>
- #include <string>
+#include <irrlicht.h>
+#include <memory>
+#include <string>
  
  namespace chessit {
  
@@ -28,9 +23,9 @@ index 1c8c3c648a14442dad41dbc92031b8800beec63d..8d06d3e63f2f9b0392a33ecfb1945375
  
  private:
      bool CreateDevice();
-+    void LoadLogoTexture();
-+    void DrawStartupSplash();
-+    void DrawScreenLogo();
+    void LoadLogoTexture();
+    void DrawStartupSplash();
+    void DrawScreenLogo();
      void StartNewGame();
  
      std::string mediaDir_;
@@ -39,7 +34,7 @@ index 1c8c3c648a14442dad41dbc92031b8800beec63d..8d06d3e63f2f9b0392a33ecfb1945375
      irr::scene::ISceneManager* sceneManager_ = nullptr;
      irr::gui::IGUIEnvironment* guiEnvironment_ = nullptr;
      irr::u32 lastFrameTimeMs_ = 0;
-+    irr::video::ITexture* logoTexture_ = nullptr;
+    irr::video::ITexture* logoTexture_ = nullptr;
  
      BoardManager boardManager_;
      ChessSceneManager chessSceneManager_;
